649848544ac3914102d2cb8f2e5d2814a0a320fe
Dennis Schulmeister-Zimolong <dennis@patk.org>
2023-03-02 14:06:12 +0100

    /app/

--- Grundgerüst einer Single Page App mit REST-Backend/start-docker-dev.bat
+++ Grundgerüst einer Single Page App mit REST-Backend/start-docker-dev.bat
@@ -5,7 +5,7 @@ echo
 echo " » Backend:  http://localhost:3000"
 echo " » Frontend: http://localhost:4000"
 echo " » DB-Admin: http://localhost:8081"
-echo " » Gateway:  http://localhost:8080/app   ← Produktivsetup der App"
+echo " » Gateway:  http://localhost:8080/app/  ← Produktivsetup der App"
 pause
 
 echo "Container werden nun gestartet. Zum Beenden Strg+C$ drücken!"
